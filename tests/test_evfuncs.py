09dd792d57a5c8972effebaf6e4086c1398ddd4a
nickledave <nickledave@users.noreply.github.com>
2018-11-22 23:19:29 -0500

    test_evfuncs loops through all files in test_data, add tests for all functions

--- tests/test_evfuncs.py
+++ tests/test_evfuncs.py
@@ -2,6 +2,7 @@
 test evfuncs module
 """
 import os
+from glob import glob
 import unittest
 
 import numpy as np
@@ -19,35 +20,45 @@ class TestEvfuncs(unittest.TestCase):
         )
 
     def test_readrecf(self):
-        rec = os.path.join(self.test_data_dir,
-                           'gy6or6_baseline_230312_0808.138.rec')
-        rec_dict = evfuncs.readrecf(rec)
-        self.assertTrue('header' in rec_dict)
-        self.assertTrue(type(rec_dict['header']) == list)
-        self.assertTrue('sample_freq' in rec_dict)
-        self.assertTrue(type(rec_dict['sample_freq']) == int)
-        self.assertTrue('num_channels' in rec_dict)
-        self.assertTrue(type(rec_dict['num_channels']) == int)
-        self.assertTrue('num_samples' in rec_dict)
-        self.assertTrue(type(rec_dict['num_samples']) == int)
-        self.assertTrue('iscatch' in rec_dict)
-        self.assertTrue('outfile' in rec_dict)
-        self.assertTrue('time_before' in rec_dict)
-        self.assertTrue(type(rec_dict['time_before']) == float)
-        self.assertTrue('time_after' in rec_dict)
-        self.assertTrue(type(rec_dict['time_after']) == float)
-        self.assertTrue('thresholds' in rec_dict)
-        self.assertTrue(type(rec_dict['thresholds']) == list)
-        self.assertTrue(all(
-            [type(thresh)==float for thresh in rec_dict['thresholds']]
-        ))
-        self.assertTrue('feedback_info' in rec_dict)
-        self.assertTrue(type(rec_dict['feedback_info']) == dict)
+        rec_files = glob(os.path.join(self.test_data_dir, '*.rec'))
+        for rec_file in rec_files:
+            rec_dict = evfuncs.readrecf(rec_file)
+            self.assertTrue('header' in rec_dict)
+            self.assertTrue(type(rec_dict['header']) == list)
+            self.assertTrue('sample_freq' in rec_dict)
+            self.assertTrue(type(rec_dict['sample_freq']) == int
+                            or type(rec_dict['sample_freq']) == float)
+            self.assertTrue('num_channels' in rec_dict)
+            self.assertTrue(type(rec_dict['num_channels']) == int)
+            self.assertTrue('num_samples' in rec_dict)
+            self.assertTrue(type(rec_dict['num_samples']) == int)
+            self.assertTrue('iscatch' in rec_dict)
+            self.assertTrue('outfile' in rec_dict)
+            self.assertTrue('time_before' in rec_dict)
+            self.assertTrue(type(rec_dict['time_before']) == float)
+            self.assertTrue('time_after' in rec_dict)
+            self.assertTrue(type(rec_dict['time_after']) == float)
+            self.assertTrue('thresholds' in rec_dict)
+            self.assertTrue(type(rec_dict['thresholds']) == list)
+            self.assertTrue(all(
+                [type(thresh) == float for thresh in rec_dict['thresholds']]
+            ))
+            self.assertTrue('feedback_info' in rec_dict)
+            self.assertTrue(type(rec_dict['feedback_info']) == dict)
 
     def test_load_cbin(self):
-        cbin = os.path.join(self.test_data_dir,
-                            'gy6or6_baseline_230312_0808.138.cbin')
-        dat, fs = evfuncs.load_cbin(cbin)
-        self.assertTrue(type(dat) == np.ndarray)
-        self.assertTrue(dat.dtype == '>i2')  # should be big-endian 16 bit
-        self.assertTrue(type(fs) == int)
+        cbins = glob(os.path.join(self.test_data_dir, '*.cbin'))
+        for cbin in cbins:
+            dat, fs = evfuncs.load_cbin(cbin)
+            self.assertTrue(type(dat) == np.ndarray)
+            self.assertTrue(dat.dtype == '>i2')  # should be big-endian 16 bit
+            self.assertTrue(type(fs) == int or type(fs) == float)
+
+    def test_load_notmat(self):
+        self.assertTrue(False)
+
+    def test_bandpass_filtfilt(self):
+        self.assertTrue(False)
+
+    def test_smooth_data(self):
+        self.assertTrue(False)
