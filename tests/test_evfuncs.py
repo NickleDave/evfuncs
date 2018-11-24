c62b1c4c8795d4d44ee7a6580d24d97616bea8d8
nickledave <nickledave@users.noreply.github.com>
2018-11-23 21:33:11 -0500

    flesh out test for load_notmat and bandpass_filtfilt

--- tests/test_evfuncs.py
+++ tests/test_evfuncs.py
@@ -55,10 +55,37 @@ class TestEvfuncs(unittest.TestCase):
             self.assertTrue(type(fs) == int)
 
     def test_load_notmat(self):
-        self.assertTrue(False)
+        notmats = glob(os.path.join(self.test_data_dir, '*.not.mat'))
+        for notmat in notmats:
+            notmat_dict = evfuncs.load_notmat(notmat)
+            self.assertTrue(type(notmat_dict) is dict)
+            self.assertTrue('onsets' in notmat_dict)
+            self.assertTrue(type(notmat_dict['onsets']) == np.ndarray)
+            self.assertTrue(notmat_dict['onsets'].dtype == float)
+            self.assertTrue('offsets' in notmat_dict)
+            self.assertTrue(type(notmat_dict['offsets']) == np.ndarray)
+            self.assertTrue(notmat_dict['offsets'].dtype == float)
+            self.assertTrue('labels' in notmat_dict)
+            self.assertTrue(type(notmat_dict['labels']) == str)
+            self.assertTrue('Fs' in notmat_dict)
+            self.assertTrue(type(notmat_dict['Fs']) == int)
+            self.assertTrue('fname' in notmat_dict)
+            self.assertTrue(type(notmat_dict['fname']) == str)
+            self.assertTrue('min_int' in notmat_dict)
+            self.assertTrue(type(notmat_dict['min_int']) == int)
+            self.assertTrue('min_dur' in notmat_dict)
+            self.assertTrue(type(notmat_dict['min_dur']) == int)
+            self.assertTrue('threshold' in notmat_dict)
+            self.assertTrue(type(notmat_dict['threshold']) == int)
+            self.assertTrue('sm_win' in notmat_dict)
+            self.assertTrue(type(notmat_dict['sm_win']) == int)
 
     def test_bandpass_filtfilt(self):
-        self.assertTrue(False)
+        cbins = glob(os.path.join(self.test_data_dir, '*.cbin'))
+        for cbin in cbins:
+            dat, fs = evfuncs.load_cbin(cbin)
+            filtsong = evfuncs.bandpass_filtfilt(dat, fs)
+            self.assertTrue(type(filtsong) == np.ndarray)
 
     def test_smooth_data(self):
         self.assertTrue(False)
