d266fffa18be7dafb40c568a87697b231bd65d91
NickleDave <nicholdav@gmail.com>
2019-11-15 08:04:27 -0500

    change __init__.py to import metadata from __about__.py

--- src/evfuncs/__init__.py
+++ src/evfuncs/__init__.py
@@ -1,1 +1,13 @@
+from .__about__ import (
+    __author__,
+    __commit__,
+    __copyright__,
+    __email__,
+    __license__,
+    __summary__,
+    __title__,
+    __uri__,
+    __version__,
+)
+
 from .evfuncs import readrecf, load_cbin, load_notmat, bandpass_filtfilt, smooth_data, segment_song
